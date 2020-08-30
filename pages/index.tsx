f326e3c26607321f6130a2cbd6aae549e68c4c3c
Mauricio Muratori <maurimura@gmail.com>
2020-08-30 15:34:12 -0300

    PWA and assetes added

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>klapaucius</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+
+            <main>
+                <h1>Klapaucius</h1>
+            </main>
+
+            <footer />
+        </div>
+    );
+}
